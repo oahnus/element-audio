25529a8abe7a15ae45e8a9d2d7e123c468e551b0
oahnus <15751774919@163.com>
2018-05-23 22:52:02 +0800

    Add Border, Color, Icon, And Native Attribute

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <template>
   <div id="app">
     <div v-for="(item, index) in audios" :key="index">
-      <VueAudio :theUrl="item.url" :theControlList="item.controlList"/>
+      <VueAudio :src="item.url"
+                :theControlList="item.controlList"
+                displayType="text"
+      ></VueAudio>
+    </div>
+
+    <div><h3>With More</h3></div>
+    <div>
+      <p>With Border</p>
+      <VueAudio :src="mp3Url"
+                border
+                theControlList="onlyOnePlaying"></VueAudio>
+    </div>
+    <div>
+      <p>With Custom Color</p>
+      <p>color: #66cc22</p>
+      <VueAudio :src="mp3Url"
+                border
+                color="#66cc22"
+                theControlList="onlyOnePlaying"></VueAudio>
+    </div>
+    <div>
+      <p>With Icon</p>
+      <VueAudio :src="mp3Url"
+                border
+                color="#66cc22"
+                displayType="normal"
+                theControlList="onlyOnePlaying"></VueAudio>
+    </div>
+    <div>
+      <p>Loop Play</p>
+      <VueAudio :src="mp3Url"
+                border
+                loop
+                color="#66cc22"
+                displayType="text"
+                theControlList="onlyOnePlaying"></VueAudio>
     </div>
   </div>
 </template>
@@ -31,7 +67,8 @@ export default {
           url: './static/horse.ogg',
           controlList: 'noDownload noSpeed onlyOnePlaying'
         }
-      ]
+      ],
+      mp3Url: './static/test.mp3'
     }
   }
 }
